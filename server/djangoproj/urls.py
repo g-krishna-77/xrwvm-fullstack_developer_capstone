12637d084e0196e77297cdbe9d98b66d833e7fb3
Krishna <krishnagokuldas77@gmail.com>
2025-10-26 00:44:16 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,11 +19,12 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html"), name='home'),
     path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
     path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
+    path('login/', TemplateView.as_view(template_name="index.html"), name='login'),
+    path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
