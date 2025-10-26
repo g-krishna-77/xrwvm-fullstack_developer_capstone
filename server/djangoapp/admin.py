ff9ffba0b31a8904df59ce8555985f99d8a15ac9
Krishna <krishnagokuldas77@gmail.com>
2025-10-26 14:22:39 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,18 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+# Inline admin to view car models under their make
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
 
 
-# Register your models here.
+# Customize CarMake Admin
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    inlines = [CarModelInline]
 
-# CarModelInline class
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+# Register both models
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
