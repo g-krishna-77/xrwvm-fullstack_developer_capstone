1bba34e4313cc8f580b17f07b0240b14e31b0d1f
Krishna <krishnagokuldas77@gmail.com>
2025-10-26 13:27:45 +0530

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -131,5 +131,5 @@ app.post('/insert_review', async (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`✅ Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
