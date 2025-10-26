9b5693d9aaafdcaccca866a172e9d444d6ffc967
Krishna <krishnagokuldas77@gmail.com>
2025-10-26 14:23:24 +0530

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,32 @@
+from .models import CarMake, CarModel
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    car_make_data = [
+        {"name": "Nissan", "description": "Great cars. Japanese technology"},
+        {"name": "Mercedes", "description": "Great cars. German technology"},
+        {"name": "Audi", "description": "German engineering excellence"},
+        {"name": "Kia", "description": "Great cars. Korean technology"},
+        {"name": "Toyota", "description": "Reliable and efficient"},
+    ]
+
+    car_make_instances = []
+    for data in car_make_data:
+        car_make_instances.append(
+            CarMake.objects.create(name=data['name'], description=data['description'])
+        )
+
+    car_model_data = [
+        {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
+        {"name": "A-Class", "type": "Sedan", "year": 2023, "car_make": car_make_instances[1]},
+        {"name": "A4", "type": "Sedan", "year": 2023, "car_make": car_make_instances[2]},
+        {"name": "Sorrento", "type": "SUV", "year": 2023, "car_make": car_make_instances[3]},
+        {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
+    ]
+
+    for data in car_model_data:
+        CarModel.objects.create(
+            name=data['name'],
+            car_make=data['car_make'],
+            type=data['type'],
+            year=data['year']
+        )
