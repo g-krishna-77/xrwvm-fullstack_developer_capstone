a8f184a9d87cf1d6e20973b30585c3a7e7118b9e
Krishna <krishnagokuldas77@gmail.com>
2025-10-26 19:26:08 +0530

    Refactor URL patterns and remove comments

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,20 +1,9 @@
-"""
-djangoproj URL Configuration
+"""djangoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+https://docs.djangoproject.com/en/3.2/topics/http/urls/
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -27,33 +16,54 @@ import os
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    
+
     # Home and static HTML pages
-    path('', TemplateView.as_view(template_name="Home.html"), name='home'),
-    path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
-    path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
-    
+    path('', TemplateView.as_view(
+        template_name="Home.html"), name='home'),
+    path('about/', TemplateView.as_view(
+        template_name="About.html"), name='about'),
+    path('contact/', TemplateView.as_view(
+        template_name="Contact.html"), name='contact'),
+
     # Authentication pages (React)
-    path('login/', TemplateView.as_view(template_name="index.html"), name='login'),
-    path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
-    
+    path('login/', TemplateView.as_view(
+        template_name="index.html"), name='login'),
+    path('register/', TemplateView.as_view(
+        template_name="index.html"), name='register'),
+
     # Dealers pages (React)
-    path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
-    path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='dealer_details'),
-    path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='postreview'),
-    
-    # Serve React manifest and meta files from build directory
+    path('dealers/', TemplateView.as_view(
+        template_name="index.html"), name='dealers'),
+    path('dealer/<int:dealer_id>/', TemplateView.as_view(
+        template_name="index.html"), name='dealer_details'),
+    path('postreview/<int:dealer_id>/', TemplateView.as_view(
+        template_name="index.html"), name='postreview'),
+
+    # Serve React manifest and meta files
     path('manifest.json', serve, {
-        'document_root': os.path.join(settings.BASE_DIR, 'frontend', 'build'),
+        'document_root': os.path.join(
+            settings.BASE_DIR, 'frontend', 'build'
+        ),
         'path': 'manifest.json'
     }),
     path('favicon.ico', serve, {
-        'document_root': os.path.join(settings.BASE_DIR, 'frontend', 'build'),
+        'document_root': os.path.join(
+            settings.BASE_DIR, 'frontend', 'build'
+        ),
         'path': 'favicon.ico'
     }),
     path('robots.txt', serve, {
-        'document_root': os.path.join(settings.BASE_DIR, 'frontend', 'build'),
+        'document_root': os.path.join(
+            settings.BASE_DIR, 'frontend', 'build'
+        ),
         'path': 'robots.txt'
     }),
-    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('logo192.png', serve, {
+        'document_root': os.path.join(
+            settings.BASE_DIR, 'frontend', 'build'
+        ),
+        'path': 'logo192.png'
+    }),
+
+] + static(settings.STATIC_URL,
+           document_root=settings.STATIC_ROOT)
