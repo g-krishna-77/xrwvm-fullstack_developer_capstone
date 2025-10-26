b073b027a06c81675002a0beb71badda9d9ab9f1
g-krishna-77 <krishnagokuldas77@gmail.com>
2025-10-26 07:24:24 -0400

    update

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -27,4 +27,9 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
     path('login/', TemplateView.as_view(template_name="index.html"), name='login'),
     path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
+    
+    # Dealers pages
+    path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
+    path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='dealer_details'),
+    path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='postreview'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
